d3aa9e0ff902daf9d29799c2c2335e6bb3b5a55d
Rajesh Varadharajan <rajeshrv@sifive.com>
2020-02-25 14:05:50 +0530

    Added the VCD dumping

--- src/main/scala/PassThru.scala
+++ src/main/scala/PassThru.scala
@@ -1,12 +1,11 @@
 
 import chisel3._
 import chisel3.util._
-import chiseltest._
-import org.scalatest._
 
  class PassThru extends MultiIOModule {
    val in = IO(Input(UInt(4.W)))
    val out = IO(Output(UInt(4.W)))
     out := in
+    //printf(p"test = $in \n")
   }
 
