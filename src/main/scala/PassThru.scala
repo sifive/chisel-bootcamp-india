cc50e24ccdf843d1cf410b24ccc3d6119b5effc8
Rajesh Varadharajan <rajeshrv@sifive.com>
2020-02-25 12:05:31 +0530

    Checked in the working version

--- src/main/scala/PassThru.scala
+++ src/main/scala/PassThru.scala
@@ -1,31 +1,12 @@
 
+import chisel3._
+import chisel3.util._
+import chiseltest._
+import org.scalatest._
 
- import chisel3._
- import chisel3.util._
- import chisel3.iotesters.{PeekPokeTester, Driver}
-
- class PassThru extends Module {
-   val io = IO (new Bundle {
-     val in = Input(UInt(4.W))
-     val out = Output(UInt(4.W))
-   })
-
-	io.out := io.in
-//   val regIn = RegInit(0.U(4.W))
-//   regIn = io.in
-//   io.out = regIn
- }
-
- class PassThruTest (c:PassThru) extends PeekPokeTester(c) {
-   poke(c.io.in, 4.U)
-   step(1)
-   expect(c.io.out,4.U)
-   step(1)
- }
-  
- object PassThruTester {
-   def main(args:Array[String]): Unit = {
-     if (!Driver(() => new PassThru(),"verilator")(c => new PassThruTest(c))) System.exit(1)
-   }
- }
+ class PassThru extends MultiIOModule {
+   val in = IO(Input(UInt(4.W)))
+   val out = IO(Output(UInt(4.W)))
+    out := in
+  }
 
---
+++ src/test/scala/PassThruTest.scala
@@ -0,0 +1,30 @@
+
+import chisel3._
+import chisel3.util._
+import chiseltest._
+import org.scalatest._
+import chiseltest.internal.WriteVcdAnnotation
+import chiseltest.internal.VerilatorBackendAnnotation
+import chisel3.tester.experimental.TestOptionBuilder._
+
+class PassThruTest extends FlatSpec with ChiselScalatestTester {
+  behavior of "PassThru Block" 
+
+  it should "work as expected" in {
+    test(new PassThru).withAnnotations(Seq(VerilatorBackendAnnotation)) { c =>
+      c.in.poke(4.U)
+      c.clock.step(10)
+      c.out.expect(4.U)
+    }
+  }
+
+  it should "Fail" in {
+    test(new PassThru).withAnnotations(Seq(VerilatorBackendAnnotation)) { c =>
+      c.in.poke(4.U)
+      c.clock.step(10)
+      c.out.expect(4.U, "Expecting 2")
+    }
+  }
+}
+
+
